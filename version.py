2df781097bb672b7c0ea966c7c677fa77c4631f7
ross-spencer <allalongthewatchtower2001@gmail.com>
2017-03-09 20:25:17 +1300

    [refactor] Cleaning up code before release.

---
+++ version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Version:
+   def getversion(self):
+      return "Version 0.0.1, Found on [GitHub [LINK]]"
+   def getcreator(self):
+      return "Maori Land Court Scanning Project"
