81d3d16fc798d381f0e356c5c4a48f1cf9ecc9df
ross-spencer <allalongthewatchtower2001@gmail.com>
2017-03-09 20:56:47 +1300

    [feature] GitHub repo added to the metadata.

--- version.py
+++ version.py
@@ -3,6 +3,6 @@
 
 class Version:
    def getversion(self):
-      return "Version 0.0.1, Found on [GitHub [LINK]]"
+      return "Version 1.0.0, Found on [GitHub [https://github.com/exponential-decay/normalize-pdfmark/releases/tag/1.0.0]]"
    def getcreator(self):
       return "Maori Land Court Scanning Project"
