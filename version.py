40fb02ca8ee4688e4d5b180b76a0094a5ccb09fe
ross-spencer <allalongthewatchtower2001@gmail.com>
2017-03-10 10:06:19 +1300

    [feature] Upping the version for release.

--- version.py
+++ version.py
@@ -3,6 +3,6 @@
 
 class Version:
    def getversion(self):
-      return "Version 1.0.0, Found on GitHub: https://github.com/exponential-decay/normalize-pdfmark/releases/tag/1.0.0"
+      return "Version 1.0.1, Found on GitHub: https://github.com/exponential-decay/normalize-pdfmark/releases/tag/1.0.1"
    def getcreator(self):
       return "Maori Land Court Scanning Project"
